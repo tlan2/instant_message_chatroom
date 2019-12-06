7a953d0a74a7d9e1d9bbbbdb0e4320946b14957c
Tom Lancaster <tlan2@pdx.edu>
2019-12-06 14:36:24 -0800

    Cleaned up client.py file

--- client.py
+++ client.py
@@ -1,89 +1,87 @@
 # Tom Lancaster and Pat Rademacher (c) December 2019
 # CS 494/594 - Internetworking Protocols - IRC Project
+# File - client.py
 
 import socket
 import select
 import sys
 
-def server_crash_test(message):
-    if not message:
-        print("\nServer is down!\n")
-        sys.exit(2)
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#if len(sys.argv) != 3: 
-#       print("Correct usage: script, IP address, port number")  DEPRECATE?
-#       exit() 
 
 server_ip_file=open("server_ip_address.txt", "r")
 if server_ip_file.mode == 'r':
     IP_address = server_ip_file.read()
-    print('server IP address you are connecting to is ' + IP_address)
-#IP_address = socket.gethostname() #str(sys.argv[1]) 
-Port = 8083 #int(sys.argv[2])
+    print('\nserver IP address you are connecting to is ' + IP_address + "\n")
+
+# ---- This line won't work on the school network for some reason ---
+#IP_address = socket.gethostname()
+
+Port = 8083 
 IP_address = '131.252.208.103' 
 server.connect((IP_address, Port)) 
 turn_user_name_on = False
 username = ''  
-username_var = 0   
-while True:
+username_var = 0
 
-    #if server.close():
-        #print("you have been disconnected from the network either through the server or an error connection\nPlease try running the program again")
-    # maintains a list of possible input streams DEPRECATE?
+def server_crash_test(message):
+    if not message:
+        print("\nServer is down!\n")
+        sys.exit()
 
-    sockets_list = [sys.stdin, server]
+try: 
 
-    """ There are two possible input situations. Either the 
+    while True:
+
+        sockets_list = [sys.stdin, server]
+
+        """ 
+        There are two possible input situations. Either the 
         user wants to give manual input to send to other people, 
         or the server is sending a message to be printed on the 
         screen. Select returns from sockets_list, the stream that 
         is reader for input. So for example, if the server wants 
         to send a message, then the if condition will hold true 
-        below.If the user wants to send a message, the else 
-        condition will evaluate as true"""
-    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+        below. If the user wants to send a message, the else 
+        condition will evaluate as true.
+        """
+        read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+        for socks in read_sockets:
+            logout = False
+            if socks == server and logout == False:
+                message = str(socks.recv(1024).decode())
+                server_crash_test(message)
+                message = message.strip()
+                message_cue = 'You have successfully signed in -- welcome to PAT CHAT! A chat room with attitude.'
+                if message_cue in message:
+                    print(message)
 
-    for socks in read_sockets:
-        logout = False
-        if socks == server and logout == False:
-            #voices = engine.getProperty('voices')
-            #engine.setProperty('voices', voices[19]) DEPRECATE?
-            message = str(socks.recv(1024).decode())
-            server_crash_test(message)
-            message = message.strip()
-            message_cue = 'You have successfully signed in -- welcome to PAT CHAT! A chat room with attitude.'
-            if message_cue in message:
-                print(message)
-
-                #username = message.replace('You have successfully signed in -- welcome to PAT CHAT!', ' ')
-                #turn_user_name_on = True
-                #username = username.strip() DEPRECATE?
+                else:
+                    print(message)
             else:
-                print(message)
-                #voice_message = str(message)
-                #engine.say(message)
-                #engine.runAndWait()
-                #engine.stop()
-        else:
-            username_output = "<" + username + ">"
-            message = sys.stdin.readline()
-            if message.strip() == "exit" or message.strip() == "EXIT":
-                logout = True
-            total_output = username_output + " " + message.strip()
-            if turn_user_name_on == False:
-                server.sendto(message.encode(),(IP_address, Port))
-            else:
-                server.sendto(message.encode(),(IP_address, Port))
-                print(total_output)
-            sys.stdout.flush()
+                username_output = "<" + username + ">"
+                message = sys.stdin.readline()
+                if message.strip() == "exit" or message.strip() == "EXIT":
+                    logout = True
+                total_output = username_output + " " + message.strip()
+                if turn_user_name_on == False:
+                    server.sendto(message.encode(),(IP_address, Port))
+                else:
+                    server.sendto(message.encode(),(IP_address, Port))
+                    print(total_output)
+                    sys.stdout.flush()
+        
         if logout == True:
             for i in range(100000):
-                message = "Thanks for using PAT CHAT! You will be missed (by some more than others.)"
+                message = "Thanks for using PAT CHAT! You will be missed (by some more than others)."
                 print(message)
             server.close()
-            exit
+            sys.exit()
+
+except KeyboardInterrupt:
+    print("\n\nThe program has crashed! Please restart and try again.\n")
+    server.close()
+    sys.exit()
+        
 server.close()
             
         
